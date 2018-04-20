e819da41685f99a74533d298edca94288a57b566
jon <jartolair17dw@ikzubirimanteo.com>
2018-04-20 13:34:05 +0200

    crear clase

---
+++ src/Clases/VotosComentarios.java
@@ -0,0 +1,27 @@
+package Clases;
+
+public class VotosComentarios {
+	private String usuario;
+	private int comentario;
+	private boolean voto;
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public int getComentario() {
+		return comentario;
+	}
+	public void setComentario(int comentario) {
+		this.comentario = comentario;
+	}
+	public boolean isVoto() {
+		return voto;
+	}
+	public void setVoto(boolean voto) {
+		this.voto = voto;
+	}
+	
+	
+}
